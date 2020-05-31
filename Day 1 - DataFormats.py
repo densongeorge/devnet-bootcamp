32fcc4320165e345991a3528ca8d54393cafbedd
mpapiset <manojpapisetty@gmail.com>
2020-05-31 18:20:22 +0530

    Update

---
+++ Day 1 - DataFormats.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+import json
+
+# a Python object (dict):
+x = {
+  "name": "Manoj",
+  "age": 29,
+  "city": "Hyderabad"
+}
+
+#format Json
+y=json.dumps(x, indent=4, separators=(";", " = "))
+
+
+# the result is a JSON string:
+print(y)
--- brower.py
+++
@@ -1,1 +0,0 @@
-print("I changed from CLI")
