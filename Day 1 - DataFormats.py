10ebb46117703f591894ca5ed6e361746cc755d8
mpapiset <manojpapisetty@gmail.com>
2020-06-01 10:20:20 +0530

    First class today:

--- Day 1 - DataFormats.py
+++ Day 1 - DataFormats.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import json
+from json2xml import json2xml
 
 # a Python object (dict):
 x = {
@@ -8,9 +9,11 @@ x = {
   "city": "Hyderabad"
 }
 
-#format Json
+#format JSON to a different representation
 y=json.dumps(x, indent=4, separators=(";", " = "))
-
 
 # the result is a JSON string:
 print(y)
+
+# Conver x to xml
+z = json2xml.Json2xml(x).to_xml()
