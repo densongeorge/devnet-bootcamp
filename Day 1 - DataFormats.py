1c5cbd3b08d5511b2941153090c2a93fcbe7dd69
mpapiset <manojpapisetty@gmail.com>
2020-06-01 15:35:33 +0530

    nothing

--- Day 1 - DataFormats.py
+++ Day 1 - DataFormats.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
 import json
-from json2xml import json2xml
 
 # a Python object (dict):
 x = {
@@ -14,6 +13,3 @@ y=json.dumps(x, indent=4, separators=(";", " = "))
 
 # the result is a JSON string:
 print(y)
-
-# Conver x to xml
-z = json2xml.Json2xml(x).to_xml()
